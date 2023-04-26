1e5e0010e4c32893fc5b58d404ef38e884cf080f
Andrei Maniur <adi.maniur@gmail.com>
2023-04-26 14:01:59 +0300

    delete request

--- index.js
+++ index.js
@@ -16,8 +16,17 @@ function createTeamRequest(team) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(team),
-  })
-    .then((r) => r.json());
+  }).then((r) => r.json());
+}
+
+function deleteTeamRequest(id) {
+  return fetch("http://localhost:3000/teams-json/delete", {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id }),
+  }).then((r) => r.json());
 }
 
 function getTeamAsHTML(team) {
@@ -27,7 +36,9 @@ function getTeamAsHTML(team) {
       <td>${team.members}</td>
       <td>${team.name}</td>
       <td>${team.url}</td>
-      <td></td>
+      <td>
+        <a data-id="${team.id}">❌</a>
+      </td>
     </tr>
   `
 }
@@ -51,8 +62,8 @@ function formSubmit(e) {
   const projectURL = $('#url').value;
 
   const team = {
-    promotion: promotion,
-    members: members,
+    promotion,
+    members,
     name: projectName,
     url: projectURL
   };
@@ -63,8 +74,24 @@ function formSubmit(e) {
   });  
 }
 
+function deleteTeam(id) {
+  console.warn('delete', id);
+  deleteTeamRequest(id).then((status) => {
+    console.warn("status", status);
+    if (status.success) {
+      window.location.reload();
+    } 
+  });
+}
+
 function initEvents() {
-  $('#editForm').addEventListener('submit', formSubmit)
+  $('#editForm').addEventListener('submit', formSubmit);
+  $('#editForm tbody').addEventListener('click', e => {
+    if (e.target.matches('a')) {
+      const id = e.target.dataset.id;
+      deleteTeam(id);
+    };
+  })
 }
 
 getTeamsRequest().then((teams) => {
